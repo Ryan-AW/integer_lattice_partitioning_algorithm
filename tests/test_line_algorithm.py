521b143328317c38713c936716f6648c23b6e3e1
Ryan-AW <ryanawhittock@outlook.com>
2024-07-28 22:36:22 +0300

    fixed unit tests

--- tests/test_line_algorithm.py
+++ tests/test_line_algorithm.py
@@ -7,9 +7,9 @@ class TestSomeFunction(unittest.TestCase):
     def test_all_small_values(self):
         for num_partitions in range(1, 101):
             for axis_len in range(num_partitions, 101):
-                print(f'{axis_len=}, {num_partitions=}')
                 output = line_partition(axis_len, num_partitions)
-                self.assertEqual(sum(output), axis_len)
+                with self.subTest(msg=f'check {axis_len=}, {num_partitions=}'):
+                    self.assertEqual(sum(output), axis_len)
 
     def test_some_random_big_values(self):
         for _ in range(100):
@@ -20,7 +20,8 @@ class TestSomeFunction(unittest.TestCase):
                 num_partitions, axis_len = axis_len, num_partitions
 
             output = line_partition(axis_len, num_partitions)
-            self.assertEqual(sum(output), axis_len)
+            with self.subTest(msg=f'check {axis_len=}, {num_partitions=}'):
+                self.assertEqual(sum(output), axis_len)
 
 
 if __name__ == '__main__':
