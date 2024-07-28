4069ead947d8a47d9c337fe26f8a4851562dfb53
Ryan-AW <ryanawhittock@outlook.com>
2024-07-28 12:09:47 +0300

    implment case of both axis length and number of partitions being odd

--- 1d_algorithm.py
+++ 1d_algorithm.py
@@ -4,3 +4,8 @@ PARTITION_LEN = 3
 NUM_PARTITIONS = 3
 
 partition_len_array = [PARTITION_LEN] * NUM_PARTITIONS
+
+
+if AXIS_LEN % 2:
+    if NUM_PARTITIONS % 2:
+        partition_len_array[NUM_PARTITIONS//2] += 1  # set middle if both the num partitions and the grid length are odd
