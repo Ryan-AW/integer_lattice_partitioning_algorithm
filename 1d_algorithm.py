4cfa29d8a66d94bc225544653be03bdcc2462676
Ryan-AW <ryanawhittock@outlook.com>
2024-07-28 12:18:23 +0300

    added for loop to increment from start and end

--- 1d_algorithm.py
+++ 1d_algorithm.py
@@ -3,9 +3,15 @@ AXIS_LEN = 10
 PARTITION_LEN = 3
 NUM_PARTITIONS = 3
 
+margin = AXIS_LEN // NUM_PARTITIONS
 partition_len_array = [PARTITION_LEN] * NUM_PARTITIONS
 
 
 if AXIS_LEN % 2:
     if NUM_PARTITIONS % 2:
         partition_len_array[NUM_PARTITIONS//2] += 1  # set middle if both the num partitions and the grid length are odd
+
+# increment from start and end of partition_len_array
+for i in range(margin//2):
+    partition_len_array[i] += 1
+    partition_len_array[-1 - i] += 1
