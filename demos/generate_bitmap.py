675b50f0bbd011b6caf334e8b000e658deec3fe5
Ryan-AW <ryanawhittock@outlook.com>
2024-07-31 00:14:57 +0300

    generate a bitmap image

--- demos/generate_bitmap.py
+++ demos/generate_bitmap.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from colorsys import hsv_to_rgb
 from algorithms.plane_algorithm import plane_partition
 
@@ -7,16 +8,40 @@ def generate_colors(num_colors: int):
         yield hsv_to_rgb(i*(1.0/num_colors), 1.0, 1.0)
 
 
-def transform(plane_xy: tuple[int, int], num_columns: int, num_rows: int):
+def generate_bitmap(filename: str, plane_xy: tuple[int, int], num_columns: int, num_rows: int):
     partition = plane_partition(plane_xy, num_columns, num_rows)
+    width, height = sum(partition['horizontal']), sum(partition['vertical'])
+
     color_array = list(generate_colors(len(partition['horizontal'])*len(partition['vertical'])))
 
     bitmap = []
     for i, num_repeats in enumerate(partition['vertical']):
         row = []
         for j, seg_length in enumerate(partition['horizontal']):
-            row += [color_array[(i*num_rows)+j]] * seg_length
+            row += [color_array[(i*len(partition['horizontal']))+j]] * seg_length
 
         for _ in range(num_repeats):
             bitmap.append(row)
-    return bitmap
+
+    img = Image.new('RGB', (width, height))
+    pixels = img.load()
+    for y in range(height):
+        for x in range(width):
+            pixels[x, y] = (
+                int(bitmap[y][x][0] * 255),
+                int(bitmap[y][x][1] * 255),
+                int(bitmap[y][x][2] * 255)
+            )
+    img.save(filename+'.bmp', 'BMP')
+
+
+generate_bitmap(
+            filename=input('Enter Filename For The Output: '),
+            plane_xy=(
+                int(input('Enter plane width: ')),
+                int(input('Enter plane height: '))
+            ),
+
+            num_columns=int(input('Enter number of columns: ')),
+            num_rows=int(input('Enter number of rows: '))
+        )
